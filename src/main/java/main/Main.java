74b11ff246d4b414bb84ccda64a4bfb1cdba3090
Adrian Małolepszy <adr_mal@wp.pl>
2015-12-08 14:08:11 +0100

    changing return type to Response

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -3,7 +3,7 @@ package main;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO Wywołanie POSTa prze jakiś client czy coś
+    	
     }
 
 }
--- src/main/java/rest/PlayersResource.java
+++ src/main/java/rest/PlayersResource.java
@@ -5,10 +5,12 @@ import java.util.Map;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import model.Player;
 import model.collections.PlayersCollection;
@@ -27,19 +29,31 @@ public class PlayersResource {
 	@Path("/{playerId}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public Player getPlayer(@PathParam("playerId") String playerId) throws Exception {
+	public Response getPlayer(@PathParam("playerId") String playerId) throws Exception {
 		if(playersCollection.isPlayerExists(playerId)) {
-			return playersCollection.getPlayer(playerId);
+			return Response.status(Response.Status.OK).entity(playersCollection.getPlayer(playerId)).build();
 		}
-		throw new Exception("użytkownik o podanym ID nie istnieje");
+		return Response.status(Response.Status.CONFLICT).entity("użytkownik o podanym ID nie istnieje").build();
 	}
 	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void postPlayer(Player player) {
-		if(playersCollection.isPlayerExists(player.getId())) {
+	public Response postPlayer(Player player) {
+		if(!playersCollection.isPlayerExists(player.getId())) {
 			playersCollection.addPlayer(player);
+			return Response.status(Response.Status.CREATED).entity(player.getId()).build();
 		}
+		return Response.status(Response.Status.CONFLICT).entity("użytkownik o podanym ID już istnieje").build();
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response putPlayerFirstName(String playerId, String playerFirstName) {
+		if(playersCollection.isPlayerExists(playerId)) {
+			// TODO modyfikacja imienia
+			return Response.status(Response.Status.CREATED).entity(playerId).build();
+		}
+		return Response.status(Response.Status.CONFLICT).entity("użytkownik o podanym ID nie istnieje").build();
 	}
 	
 }
